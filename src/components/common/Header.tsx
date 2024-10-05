fc4b3260523c47f0e563fae4473a5a4a54cc63cd
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-05 17:37:40 +0300

    style: :lipstick: add shadow to header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -17,7 +17,10 @@ const Logo = () => (
 
 const NavigationItem = ({ to, children }: TNavigationItemProps) => (
   <li className="w-full lg:w-auto   ">
-    <Link className="text-gray-600 hover:bg-gray-200 w-full py-3 px-4  block" to={to}>
+    <Link
+      className="text-gray-600 hover:bg-gray-200 w-full py-3 px-4  block"
+      to={to}
+    >
       {children}
     </Link>
   </li>
@@ -32,7 +35,7 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="bg-gray-100 position-fixed w-full z-3">
+      <nav className="bg-gray-100 position-fixed w-full z-3 shadow-lg">
         <div className="px-8 flex items-center justify-between">
           <div className="flex items-center gap-4 relative">
             <button className={cn("lg:hidden")} onClick={toggleMenu}>
@@ -41,11 +44,14 @@ const Header = () => {
             <Logo />
           </div>
           <ul
-            className={cn("flex items-center lg:flex-row lg:bg-transparent lg:top-0 lg:p-0 lg:relative lg:w-auto lg:items-center lg:border-none mx-auto", {
-              hidden: !isMenuOpen,
-              "lg:flex flex-col p-4 absolute left-0 top-16 bg-gray-100 w-full items-start border-t-2 ":
-                true,
-            })}
+            className={cn(
+              "flex items-center lg:flex-row lg:bg-transparent lg:top-0 lg:p-0 lg:relative lg:w-auto lg:items-center lg:border-none mx-auto",
+              {
+                hidden: !isMenuOpen,
+                "lg:flex flex-col p-4 absolute left-0 top-16 bg-gray-100 w-full items-start border-t-2 ":
+                  true,
+              }
+            )}
           >
             <NavigationItem to="/">الرئيسية</NavigationItem>
             <NavigationItem to="/about">مبادراتنا</NavigationItem>
